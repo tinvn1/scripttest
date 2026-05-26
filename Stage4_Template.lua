1261cbf846fb2ceaa2432585f7b6b0027c6a754a
tinvn1 <50511703+tinvn1@users.noreply.github.com>
2026-05-27 00:51:27 +0700

    Create Stage4_Template.lua

---
+++ Stage4_Template.lua
@@ -0,0 +1,6 @@
+-- Nội dung file Stage4_Template.lua trên GitHub của bạn
+print("[🛠️ STAGE 4] Nhân vật đã đến khúc cuối an toàn!")
+while true do
+    print("[⏳ HOLDING] Đang đứng im tại trạm điện chờ chủ nhân viết tiếp code tính năng Stage 4...")
+    task.wait(5)
+end
